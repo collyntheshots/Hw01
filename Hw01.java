0246917f80f1b036fcf430c0e3197e56579fa7dc
Collyn Lindley <clindley@knights.ucf.edu>
2020-01-26 21:19:33 -0500

    Completed insert, search, and print methods --- Still need to complete delete method and output formating

--- Hw01.java
+++ Hw01.java
@@ -4,21 +4,26 @@ import java.util.*;
 class Node
 {
 	int data;
-	Node rightChild;
-	Node leftChild;
+	Node rightChild, leftChild;
 	public Node(int data) { this.data = data; }
 }
 
 class BST
 {
-	public BST()
-	{
-		System.out.println("BST created!!!");
-	}
+	Node root;
 
 	public void insert(int data)
 	{
-		System.out.println("inserted" + data);
+		this.root = insertData(this.root, data);
+		System.out.println("inserted " + data);
+	}
+
+	private Node insertData(Node root, int data)
+	{
+		if (root == null)
+			return new Node(data);
+
+			return searchData(root, data);
 	}
 
 	public void delete(int data)
@@ -28,14 +33,39 @@ class BST
 
 	public void search(int data)
 	{
-		System.out.println("searched" + data);
+		Node root = searchData(this.root, data);
+		System.out.println("searched for " + data);
+		System.out.println("Found " + root.data);
+	}
+
+	private Node searchData(Node root, int data)
+	{
+		if (this.root == null || this.root.data == data)
+			return this.root;
+
+		if (data < root.data)
+			root.leftChild = insertData(root.leftChild, data);
+		else if (data >= root.data)
+			root.rightChild = insertData(root.rightChild, data);
+
+		return root;
 	}
 
 	public void print()
 	{
+		inOrder(this.root);
 		System.out.println("printed the whole tree");
 	}
 
+	private void inOrder(Node root)
+	{
+		if (root != null)
+		{
+			inOrder(root.leftChild);
+			System.out.println(root.data);
+			inOrder(root.rightChild);
+		}
+	}
 }
 
 public class Hw01
@@ -94,9 +124,3 @@ public class Hw01
 
 */
 }
-
-
-
-
-
-hello world
